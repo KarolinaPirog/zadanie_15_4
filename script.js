30989deaac352a6c196b183603db49c8933367c6
Karolina Piróg <karolina.swieca@wp.pl>
2018-08-13 22:28:51 +0200

    change const average ang grades

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ const multiply = (a = 1, b = 1) => console.log(a*b);
 multiply(2,3);
 
 //#3
-const average = (...args) => console.log((args => args += args)/args.length);
-average(1,2,3);
+const average = (...args) => console.log((args.reduce(function(a, b) {return a + b }))/args.length);
+average(3,1,6,5,4,5);
 
 //#4
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+average(...grades);
 
 //#5
 const myTable = [1, 4, 'Iwona', false, 'Nowak'];
